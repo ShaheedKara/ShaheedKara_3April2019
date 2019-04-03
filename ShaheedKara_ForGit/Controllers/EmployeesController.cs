498b2d516336faaa85119c47977e3f0b9340b858
Muhammad Shaheed Kara <21609959@dut4life.ac.za>
2019-04-03 21:57:55 +0200

    Add project files.

---
+++ ShaheedKara_ForGit/Controllers/EmployeesController.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using ShaheedKara_ForGit.Models;
+
+namespace ShaheedKara_ForGit.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private DataContext db = new DataContext();
+
+        // GET: Employees
+        public async Task<ActionResult> Index(string SearchValue)
+        {
+            //Seaching Index using Name or Department
+            var Names = from N in db.Employees select N;
+
+            if(!string.IsNullOrEmpty(SearchValue))
+            {
+                Names = Names.Where(x => x.FirstName.Contains(SearchValue) || x.LastName.Contains(SearchValue) ||x.Department.Contains(SearchValue));
+            }
+
+            return View(await Names.ToListAsync());
+        }
+
+        public ActionResult DisplayWage()
+        {
+            var ViewM = new WageViewModel();
+
+            Employee em = new Employee();
+            ViewM.display = em.CalcTotWage();
+            
+
+            return View(ViewM);
+        }
+
+        // GET: Employees/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+        
+
+
+            
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EmpID,FirstName,LastName,IDNumber,CellNum,Department,Wage")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Employees.Add(employee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(employee);
+        }
+
+        // GET: Employees/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EmpID,FirstName,LastName,IDNumber,CellNum,Department,Wage")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Employee employee = db.Employees.Find(id);
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ShaheedKara_ForGit/Models/Employee.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ShaheedKara_ForGit.Models
+{
+    public class Employee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name ="Employee ID")]
+        public int EmpID { get; set; }
+        [Required]
+        [Display(Name = "Employee First Name")]
+        public String FirstName { get; set; }
+        [Required]
+        [Display(Name = "Employee Last Name")]
+        public String LastName { get; set; }
+        [Required]
+        [Display(Name = "ID Number")]
+        public String IDNumber { get; set; }
+        [Required]
+        [Display(Name = "Employee Cellphone Number")]
+        public String CellNum { get; set; }
+        [Required]
+        [Display(Name = "Employee Department")]
+        public String Department { get; set; }
+        [Required]
+        [Display(Name = "Employee Wage")]
+        public double Wage { get; set; }
+
+
+
+
+
+        public string CalcTotWage()
+        {
+
+
+            DataContext db = new DataContext();
+            double Total = 0;
+            Employee emp = new Employee();
+            var Totwage = from w in db.Employees select w.Wage;
+            Totwage.ToList();
+             Total = Totwage.Sum();
+            double average = Totwage.Average();
+
+            string output = " Total wages    :   " + Total +
+                            "\n Average wage   :   " + average;
+
+
+            return output;
+        }
+    }
+
+    public class WageViewModel
+    {
+        [Key]
+        public int Key { get; set; }
+        public string display { get; set; }
+
+    }
+
+
+
+}
